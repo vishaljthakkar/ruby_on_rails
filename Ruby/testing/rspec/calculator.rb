41ea9b5d55026df7d0fdc1fedf7f121e9013ee02
Vishal Thakkar <vishaljthakkar@gmail.com>
2019-06-30 16:55:38 -0700

    Ruby RSpec example

---
+++ Ruby/testing/rspec/calculator.rb
@@ -0,0 +1,25 @@
+class Calculator
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def add (one, two)
+    #one - two
+    one + two
+  end
+
+  def subtract (one, two)
+    #one + two
+    one - two
+  end
+
+  def multiply (one, two)
+    one * two
+  end
+
+  def divide (one, two)
+    one / two
+  end
+end
---
+++ Ruby/testing/rspec/spec/calculator_spec.rb
@@ -0,0 +1,19 @@
+require 'rspec'
+require_relative '../calculator'
+
+describe Calculator do
+  before { @calculator = Calculator.new('RSpec calculator')}
+
+  it "should add 2 numbers correctly" do
+    expect(@calculator.add(2, 2)).to eq 4
+  end
+
+  it "should subtract 2 numbers correctly" do
+    expect(@calculator.subtract(4, 2)).to eq 2
+  end
+
+  it "should sum two odd numbers and be even" do
+    expect(@calculator.subtract(3, 3)).to be_even
+    expect(@calculator.subtract(3, 3)).not_to be_odd
+  end
+end
