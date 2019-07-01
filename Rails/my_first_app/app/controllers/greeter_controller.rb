583aac92d91ab6e2495864747c5b04fbd461ff93
Vishal Thakkar <vishaljthakkar@gmail.com>
2019-07-01 00:17:29 -0700

    Rails: Use helper to add formatted time. This helper is not just available to the greeter view but rather avaialbe to all views. * using link_to => when using it there is no need to change things when the path change. rake routes                    Prefix Verb URI Pattern                                                                              Controller#Action             greeter_hello GET  /greeter/hello(.:format)                                                                 greeter#hello           greeter_goodbye GET  /greeter/goodbye(.:format)                                                               greeter#goodbye        rails_service_blob GET  /rails/active_storage/blobs/:signed_id/*filename(.:format)                               active_storage/blobs#show rails_blob_representation GET  /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show        rails_disk_service GET  /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show update_rails_disk_service PUT  /rails/active_storage/disk/:encoded_token(.:format)                                      active_storage/disk#update      rails_direct_uploads POST /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create

--- Rails/my_first_app/app/controllers/greeter_controller.rb
+++ Rails/my_first_app/app/controllers/greeter_controller.rb
@@ -1,13 +1,14 @@
+# frozen_string_literal: true
+
 class GreeterController < ApplicationController
 
   def hello
-    random_names = ["Alex", "Joe", "Michael"]
+    random_names = %w[Alex Joe Michael]
     @name = random_names.sample
     @time = Time.now
     @times_displayed ||= 0
     @times_displayed += 1
   end
 
-  def goodbye
-  end
+  def goodbye; end
 end
--- Rails/my_first_app/app/helpers/greeter_helper.rb
+++ Rails/my_first_app/app/helpers/greeter_helper.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 module GreeterHelper
+  def formatted_time(time)
+    # Time string in AM/PM format
+    time.strftime('%I:%M%p')
+  end
 end
