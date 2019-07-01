459c70c4bdd36e6ad84aff04f87070a09effe0ce
Vishal Thakkar <vishaljthakkar@gmail.com>
2019-06-30 23:16:42 -0700

    Rails: Add route for greeter action. * New commands: rake --tasks => This will list all rake tasks * rake --describe <taskname> => rake --describe routes * rake routes => shows all rest endpoint  your application supports

--- Rails/my_first_app/config/routes.rb
+++ Rails/my_first_app/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'greeter/hello'
+  # get 'greeter/goodbye'
+  get 'greeter/goodbye' => 'greeter#goodbye'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
