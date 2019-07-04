baba05864f42930844ac44533636ea892e7bc985
Vishal Thakkar <vishaljthakkar@gmail.com>
2019-07-04 16:23:53 -0700

    Advanced Active Record: Example of validates

--- Advanced_ActiveRecord/advanced_ar/app/models/job.rb
+++ Advanced_ActiveRecord/advanced_ar/app/models/job.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 class Job < ApplicationRecord
   belongs_to :person
   has_one :salary_range
+
+  validates :title, :company, presence: true
 end
--- Advanced_ActiveRecord/advanced_ar/app/models/salary_range.rb
+++ Advanced_ActiveRecord/advanced_ar/app/models/salary_range.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
 class SalaryRange < ApplicationRecord
   belongs_to :job
+
+  # built in :numericality can do this. But this is just to show as an example.
+  validates_numericality_of :min_salary, less_than: :max_salary
+
 end
