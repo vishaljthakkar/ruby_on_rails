8dd2b4971accecffb49aec84a261fdcdbf417d72
Vishal Thakkar <vishaljthakkar@gmail.com>
2019-07-04 16:10:21 -0700

    Advanced Active Record: Example of default scope and named scope

--- Advanced_ActiveRecord/advanced_ar/app/models/person.rb
+++ Advanced_ActiveRecord/advanced_ar/app/models/person.rb
@@ -8,10 +8,16 @@ class Person < ApplicationRecord
   has_and_belongs_to_many :hobbies
   has_many :approx_salaries, through: :jobs, source: :salary_range
 
+  default_scope { order :first_name }
   # For an instance of a person, what is his max salary.
   # take approx_salaries array, then calculate maximum of those
   # and maximum should be using the max_salary column of salary_range
   def max_salary
     approx_salaries.maximum(:max_salary)
   end
+
+  # SCOPE has to return Active Record Relation vs Classmethod style max_salary above
+  # scope :ordered_by_age, -> { order(age: :desc) }
+  scope :ordered_by_age, -> { order age: :desc }
+  scope :starts_with, ->(starting_string){ where('first_name LIKE ?', "#{starting_string}%") }
 end
