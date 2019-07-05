9d545e2b048f54ad33b0d41244fc0f0f38daa1ac
Vishal Thakkar <vishaljthakkar@gmail.com>
2019-07-05 11:32:12 -0700

    Security and Nested Resource: Add Notes controller and in that make sure we have book before we do any action on notes. Take a good look at the file.

--- Security_Nested_Resource/i_reviewed/config/routes.rb
+++ Security_Nested_Resource/i_reviewed/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # resources gives all 7 actions but we need only 2 for notes for now
   resources :books do
-    resources :notes # nested resources.
+    resources :notes, only: [:create, :destroy] # nested resources.
   end
 
   root 'books#index'
