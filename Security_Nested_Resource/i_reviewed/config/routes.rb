294b012880f58a02baccd146de69ae64d0911ef6
Vishal Thakkar <vishaljthakkar@gmail.com>
2019-07-05 11:19:05 -0700

    Security and Nested Resource: Nested resource example. book has many notes

--- Security_Nested_Resource/i_reviewed/config/routes.rb
+++ Security_Nested_Resource/i_reviewed/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :books
+  resources :books do
+    resources :notes # nested resources.
+  end
+
   root 'books#index'
 end
