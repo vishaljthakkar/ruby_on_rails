65a44ec8893022a6e60a1be82742ecded4dc0a17
Vishal Thakkar <vishaljthakkar@gmail.com>
2019-07-05 14:49:58 -0700

    Security and Nested Resource: Add login form at http://localhost:3000/login * Implement new, create and destroy action for sessions controller. * Open up the URL and inspect the page and network to understand html and api called on submit and verify that cookie has i_reviewed_session filled in

--- Security_Nested_Resource/i_reviewed/app/controllers/sessions_controller.rb
+++ Security_Nested_Resource/i_reviewed/app/controllers/sessions_controller.rb
@@ -1,10 +1,32 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
   def new
+    # Login Page - new.html.erb
   end
 
   def create
+    reviewer = Reviewer.find_by(name: params[:reviewer][:name])
+    password = params[:reviewer][:password]
+
+    # if reviewer && reviewer.authenticate(password)
+    #   session[:reviewer_id] = reviewer.id
+    #   redirect_to root_path, notice: "Logged in successfully"
+    # else
+    #   redirect_to login_path, alert: "Invalid username/password combination"
+    # end
+    # Rubocop suggested safe navigation of object instead of checking like above
+    #
+    if reviewer&.authenticate(password)
+      session[:reviewer_id] = reviewer.id
+      redirect_to root_path, notice: 'Logged in successfully'
+    else
+      redirect_to login_path, alert: 'Invalid username/password combination'
+    end
   end
 
   def destroy
+    reset_session # rails helper wipe out session and everything in it
+    redirect_to login_path, notice: 'You have been logged out.'
   end
 end
--- Security_Nested_Resource/i_reviewed/app/models/reviewer.rb
+++ Security_Nested_Resource/i_reviewed/app/models/reviewer.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class Reviewer < ApplicationRecord
   has_secure_password
-  
+
   has_many :books
 end
